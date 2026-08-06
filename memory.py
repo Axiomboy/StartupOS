a17090dc5840fc2273911571bae3c3d94e33f4bd
Axiomboy <vkpmom@gmail.com>
2026-08-06 12:19:06 -0700

    Add check for MEMORY_FILE existence in load_memory

--- memory.py
+++ memory.py
@@ -1,9 +1,13 @@
 import json
+import os
 
 MEMORY_FILE = "memorys.json"
 
-
 def load_memory():
+    if not os.path.exists(MEMORY_FILE):
+        with open(MEMORY_FILE, "w") as f:
+            json.dump({}, f)
+
     with open(MEMORY_FILE, "r") as f:
         return json.load(f)
 
